17ec81c917d850fcb56cb0e4554691e7f8bb0d0e
wigarbadri <wigarbadri@gmail.com>
2023-07-04 17:10:19 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  const rightBoundary = 400; // Assuming the container width is 400px
+
+  if (left + 40 < rightBoundary) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+
+
